68c1b3380187460bded17d4d780905671fb38119
tomaszosk <t.oskroba@icloud.com>
2021-05-05 23:06:22 +0200

    Tab Bar Controller added

---
+++ AppStoreClone/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreClone
+//
+//  Created by Tomasz Oskroba on 5/5/21.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
