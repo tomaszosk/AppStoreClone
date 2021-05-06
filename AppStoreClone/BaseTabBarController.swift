0ab0e01cfa19882a15292d2ddccbe20c51ed8cdf
tomaszosk <t.oskroba@icloud.com>
2021-05-06 19:59:51 +0200

    Basic setup

--- AppStoreClone/BaseTabBarController.swift
+++ AppStoreClone/BaseTabBarController.swift
@@ -12,6 +12,25 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .red
+        redViewController.navigationItem.title = "APPS"
+        
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "RED NAV"
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .blue
+        blueViewController.navigationItem.title = "SEARCH"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "BLUE NAVE"
+        blueNavController.navigationBar.prefersLargeTitles = true
+        
+        viewControllers = [
+            redNavController,
+            blueNavController
+        ]
     }
 }
