7a0db4402231e0ebf865abeb1d81761656fbb585
tomaszosk <t.oskroba@icloud.com>
2021-05-08 20:51:26 +0200

    viewDidLoad refactored

--- AppStoreClone/BaseTabBarController.swift
+++ AppStoreClone/BaseTabBarController.swift
@@ -11,26 +11,24 @@ class BaseTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let redViewController = UIViewController()
-        redViewController.view.backgroundColor = .red
-        redViewController.navigationItem.title = "APPS"
-        
-        let redNavController = UINavigationController(rootViewController: redViewController)
-        redNavController.tabBarItem.title = "RED NAV"
-        redNavController.navigationBar.prefersLargeTitles = true
-        
-        let blueViewController = UIViewController()
-        blueViewController.view.backgroundColor = .blue
-        blueViewController.navigationItem.title = "SEARCH"
-        
-        let blueNavController = UINavigationController(rootViewController: blueViewController)
-        blueNavController.tabBarItem.title = "BLUE NAVE"
-        blueNavController.navigationBar.prefersLargeTitles = true
-        
+    
         viewControllers = [
-            redNavController,
-            blueNavController
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "search")
         ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        
+        
+        return navController
     }
 }
--- AppStoreClone/SceneDelegate.swift
+++ AppStoreClone/SceneDelegate.swift
@@ -13,12 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
         
-        window?.rootViewController = BaseTabBarController()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let navController = BaseTabBarController()
+            window.rootViewController = navController
+            
+            self.window = window
+            window.makeKeyAndVisible()
+        }
         
-        
-
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
